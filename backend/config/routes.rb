7126422c18091622d7b616444b39d24a7a745e91
yushi127 <80163799+yushi127@users.noreply.github.com>
2021-09-01 19:47:48 +0900

    ユーザーidが取得できないバグ修正しました

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -5,12 +5,13 @@ Rails.application.routes.draw do
   # post "/index/create" => "plan#create"
   get 'allplans/:id/users' , to:'allplans#user', as: :userid
   post 'allplans/:id/users' , to:'allplans#usercreate'
-  get 'allplans/:id/users/:user_id/decdates' , to:'decdates#index', as: :decdateindex
-  post 'allplans/:id/users/:user_id/decdates' , to:'decdates#create'
+  # get 'allplans/:id/users/:user_id/decdates' , to:'decdates#index', as: :decdateindex
+  # post 'allplans/:id/users/:user_id/decdates' , to:'decdates#create'
 
   root 'allplans#index'
   resources :allplans do
     resources :days
+
     resources :users do
       resources :decdates , only: [:new, :show, :edit, :update, :destroy]
     end
