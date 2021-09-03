788d8581791e9a46d72ba7ed45bf18f460e6490f
KaiKohlman <kaikohlman@gmail.com>
2021-09-04 01:32:37 +0900

    ◯×つけたけど雑魚い

--- frontend/pages/projects/dateAdjust.vue
+++ frontend/pages/projects/dateAdjust.vue
@@ -1,33 +1,48 @@
 <template>
   <div>
-    <div class="form">日程調整フォーム</div><br>
+    <div class="form">日程調整フォーム</div>
+    <br />
     <div class="input">〇〇さんの予定を追加してください</div>
-  </div>
+    <div class="container">
+      <div class="radio">
+        <input id="radio-1" name="radio" type="radio" checked />
+        <label for="radio-1" class="radio-label">◯</label>
+      </div>
+
+      <div class="radio">
+        <input id="radio-2" name="radio" type="radio" />
+        <label for="radio-2" class="radio-label">×</label>
+      </div>
+      </div>
+    </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {}
 </script>
 
 <style scoped>
 * {
-    color:#666666;
-    text-align:center;
-  }
+  color: #666666;
+  text-align: center;
+}
 
-  .form {
-  color: #47525E;
+.form {
+  color: #47525e;
   font-family: Lato;
   font-size: 40px;
   line-height: 52px;
 }
 .input {
-  color: #47525E;
+  color: #47525e;
   font-family: Lato;
   font-size: 36px;
   line-height: 46px;
+}
+.container {
+display: flex;
+margin: 10% 50%;
+font-size: 1.25em;
 }
 
 </style>
