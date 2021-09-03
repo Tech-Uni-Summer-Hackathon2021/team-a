59b8bf45e2fa4ff66f4be990cc10b17cad395f95
KaiKohlman <kaikohlman@gmail.com>
2021-09-03 16:58:54 +0900

    イベント追加ページの作成

---
+++ frontend/components/MyButton.vue
@@ -0,0 +1,56 @@
+<template>
+  <a
+    class="MyButton"
+    :style="{ 'background-color': back_color, color: fore_color }"
+    :class="{ left_arrow, right_arrow }"
+    ><slot>ボタン</slot></a
+  >
+  <!-- ボタン表示(デフォルト）は各ページのボタン出したい部分にこれ書くだけ→<MyButton/>-->
+  <!--<MyButton back_color="#FD99A1">次へ</MyButton>-->
+  <!-- ↑背景色と中の文字を設定できる-->
+</template>
+<script>
+export default {
+  props: {
+    back_color: {
+      type: String,
+      default: '#666666',
+    },
+    fore_color: {
+      type: String,
+      default: '#FFFFFF',
+    },
+  },
+}
+</script>
+<style scoped>
+.MyButton {
+  display: inline-block;
+  border-radius: 5px;
+  box-shadow: 0 0.2em 0.5em rgba(0, 0, 0, 0.2);
+  padding: 2.5% 14%;
+  font-weight: bold;
+  text-decoration: none;
+  font-size: 36px;
+  text-align: center;
+}
+/*使わんと思うけど一応おいとく。矢印をボタンに追加できる機能 */
+.MyButton.left_arrow {
+  padding-left: 0;
+}
+.MyButton.left_arrow::before {
+  content: '';
+  padding-right: 1em;
+}
+.MyButton.right_arrow {
+  padding-right: 0;
+}
+.MyButton.right_arrow::after {
+  padding-left: 1em;
+  content: '〉';
+}
+/*<無効な <input> のとき、ボタンを透明度50％に -->*/
+input:disabled {
+  opacity: 0.5;
+}
+</style>
---
+++ frontend/pages/projects/eventPlan.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+    <h1>具体的なイベント案、または店内の候補を挙げてください</h1>
+    <p>提出された候補の中から、ランダムで決定します<br>
+    希望の選択肢が既に挙げられている場合も、決定する確率を上げるため同じ内容のものを入力してください。</p>
+    <h2>現在挙がっている案</h2>
+    <div></div>
+    <div></div>
+    <MyButton>追加</MyButton>
+    <MyButton>→</MyButton>
+  </div>
+</template>
+<script>
+import MyButton from "@/components/MyButton.vue";
+export default {
+  components: {
+    MyButton
+  }
+};
+</script>
+<style>
+    * {
+    color:#666666;
+    text-align:center;
+   }
+    h1{
+        font-size:40px;
+        background: linear-gradient(transparent 50%, #FD99A1  50%);
+    }
+    h2{
+        font-size:36px; 
+    }
+</style>
